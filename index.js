b3102dbceb6bf910122f83d93096d40cf5938005
Snugglez <32309975+Snugglez@users.noreply.github.com>
2019-04-25 22:06:53 -0400

    v81 update

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ module.exports = function AutoCamera(mod) {
     }
   });
 
-  mod.hook('S_LOGIN', 12, { order: -1000 }, (e) => {
+  mod.hook('S_LOGIN', mod.majorPatchVersion >= 81 ? 13 : 12, { order: -1000 }, (e) => {
     playerName = e.name;
     setDistance = data.defaultDistance;
     for (let i = 0, n = data.characterDefault.length; i < n; i++) {
